4c95c4e878cab988be27057a942428bf6dc73930
mednologic <mednologic@gmail.com>
2022-05-31 08:29:31 +0200

    "visual changes"
    Logo now its and image, the background its al cover, and i set and animation of entrance for the logo

---
+++ js/code.js
@@ -0,0 +1,6 @@
+/*Screen size*/
+    var width = window.innerWidth;
+    var height = window.innerHeight;
+    //Seting the parameters on CSS styles
+    document.documentElement.style.setProperty('--my-screenwidth', width);
+    document.documentElement.style.setProperty('--my-screenheight', height);
